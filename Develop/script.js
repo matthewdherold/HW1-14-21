d86c0ae88aaacb859ac5a7a29df2854536fd4322
Matt H <matthewdherold@gmail.com>
2021-01-07 22:57:27 -0800

    finished coding for returning the password

--- Develop/script.js
+++ Develop/script.js
@@ -10,19 +10,20 @@ function writePassword() {
 }
 
 function generatePassword() {
+  password = ""
   for (var i = 1; i < 2;) {
-  var pwlength = parseInt(prompt('How many chars? (please input a number between 8 and 128)'));
-  if (pwlength < 8) {
+  var pwLength = parseInt(prompt('How many chars? (please input a number between 8 and 128)'));
+  if (pwLength < 8) {
     alert('Password must be at least 8 chars');
   }
-  else if (pwlength > 128) {
+  else if (pwLength > 128) {
     alert('Password must be less than 128 chars');
    }
    else { i++
    }
   }
   console.log('Password length:');
-  console.log(pwlength);
+  console.log(pwLength);
   var pwlowercase = confirm('Use lowercase letters?');
   console.log('Lowercase?:');
   console.log(pwlowercase);
@@ -35,6 +36,51 @@ function generatePassword() {
   var pwspecialchars = confirm('Use special characters?');
   console.log('Special characters?:');
   console.log(pwspecialchars);
+
+  var pwCharSet = "";
+  var lowerSet = "abcdefghijklmnopqrstuvwxyz"
+  var upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberSet = "0123456789"
+  var specSet = "!#$%&*+,-./:;<=>?@^_~"
+  var criteriaSet = 0
+
+  if (pwlowercase === true) {
+    pwCharSet += lowerSet;
+    console.log(pwCharSet);
+    password += lowerSet.charAt(Math.random() * lowerSet.length);
+    console.log(password);
+    criteriaSet + 1;
+  }
+  if (pwuppercase === true) {
+    pwCharSet += upperSet;
+    console.log(pwCharSet);
+    password += upperSet.charAt(Math.random() * upperSet.length);
+    console.log(password);
+    criteriaSet + 1;
+  }
+  if (pwnumbers === true) {
+    pwCharSet += numberSet;
+    console.log(pwCharSet);
+    password += numberSet.charAt(Math.random() * numberSet.length);
+    console.log(password);
+    criteriaSet + 1;
+  }
+  if (pwspecialchars === true) {
+    pwCharSet += specSet;
+    console.log(pwCharSet);
+    password += specSet.charAt(Math.random() * specSet.length);
+    console.log(password);
+    criteriaSet + 1;
+  }
+  
+  for (var i = criteriaSet, n = pwCharSet.length; i < pwLength; ++i) {
+    password += pwCharSet.charAt(Math.random() * n);
+}
+  console.log(password);
+      
+
+
+  return password;
   
 }
 
